8f3cb8c076ba47d181a186017869d14596782583
taylorferran <tailzpk@hotmail.co.uk>
2022-11-06 14:09:25 +0700

    Finish frontend functionality

---
+++ frontend/frontend/constants/index.js
@@ -0,0 +1,390 @@
+export const MONEYMATCHES_ADDRESS = "0x67624afC73c953B69Bc0d9C5C3c829253BeC75D7"
+
+export const abi = [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "id",
+          "type": "bytes32"
+        }
+      ],
+      "name": "ChainlinkCancelled",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "id",
+          "type": "bytes32"
+        }
+      ],
+      "name": "ChainlinkFulfilled",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "id",
+          "type": "bytes32"
+        }
+      ],
+      "name": "ChainlinkRequested",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferRequested",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        }
+      ],
+      "name": "OwnershipTransferred",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "bytes32",
+          "name": "requestId",
+          "type": "bytes32"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "winner",
+          "type": "string"
+        }
+      ],
+      "name": "RequestWinner",
+      "type": "event"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "fallback"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_gameID",
+          "type": "uint256"
+        }
+      ],
+      "name": "acceptGame",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "acceptOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_gameID",
+          "type": "uint256"
+        }
+      ],
+      "name": "cancelGameBeforeItHasBeenAccepted",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "cancelMatchBeingProcessed",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_wager",
+          "type": "uint256"
+        }
+      ],
+      "name": "createGame",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes32",
+          "name": "_requestId",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "string",
+          "name": "_winner",
+          "type": "string"
+        }
+      ],
+      "name": "fulfill",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "gameSettled",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "matchBeingProcessed",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "matchList",
+      "outputs": [
+        {
+          "internalType": "address payable",
+          "name": "Hero",
+          "type": "address"
+        },
+        {
+          "internalType": "address payable",
+          "name": "Villain",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "wager",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address",
+          "name": "winner",
+          "type": "address"
+        },
+        {
+          "internalType": "bool",
+          "name": "paidOut",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "playerCurrentMatch",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "ran",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_gameID",
+          "type": "uint256"
+        }
+      ],
+      "name": "requestWinnerFromGameID",
+      "outputs": [
+        {
+          "internalType": "bytes32",
+          "name": "requestId",
+          "type": "bytes32"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_gameID",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "_winner",
+          "type": "string"
+        }
+      ],
+      "name": "settleGame",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "testOne",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "testTwo",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        }
+      ],
+      "name": "transferOwnership",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdrawLink",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "receive"
+    }
+  ]
--- frontend/frontend/pages/index.js
+++ frontend/frontend/pages/index.js
@@ -1,71 +1,278 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { providers, Contract, utils, BigNumber } from "ethers";
+import { useEffect, useRef, useState } from "react";
+import { MONEYMATCHES_ADDRESS, abi } from "../constants";
 
 export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef();
+  const [viewWagerAmount, setWagerAmount] = useState("test");
+  const [viewGameID, setGameID] = useState("test");
+
+  const wagerAmount = event => {
+    setWagerAmount(event.target.value);
+  };
+
+  const gameID = event => {
+    setGameID(event.target.value);
+  };
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Goerli network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change network to Goerli");
+      throw new Error("Change network to Goerli");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+
+  const createGame = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const moneymatchesContract = new Contract(
+        MONEYMATCHES_ADDRESS,
+        abi,
+        signer
+      );
+      
+      const oneEther = BigNumber.from("1000000000000000000");
+      //const value = utils.formatEther(viewWagerAmount);
+
+      
+      const value = utils.parseUnits(viewWagerAmount, "ether");
+      const tx = await moneymatchesContract.createGame(value, {
+        value: value,
+      });
+      
+      await tx.wait();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const acceptGame = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+
+      const moneymatchesContract = new Contract(
+        MONEYMATCHES_ADDRESS,
+        abi,
+        signer
+      );
+      
+      const gameData = await moneymatchesContract.matchList(String(viewGameID));
+
+      //const value = 0.001 * 1;
+      const tx = await moneymatchesContract.acceptGame(String(viewGameID), {
+        value: gameData[2],
+      });
+
+      await tx.wait();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+     const viewGame = async () => {
+      try {
+        const provider = await getProviderOrSigner();
+        const moneymatchesContract = new Contract(
+          MONEYMATCHES_ADDRESS,
+          abi,
+          provider
+        );
+        const tx = await moneymatchesContract.matchList(String(viewGameID));
+
+        if(tx) {
+          alert(tx);
+        }
+        alert
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const settleGame = async () => {
+      try {
+        const signer = await getProviderOrSigner(true);
+  
+        const moneymatchesContract = new Contract(
+          MONEYMATCHES_ADDRESS,
+          abi,
+          signer
+        );
+  
+        const tx = await moneymatchesContract.requestWinnerFromGameID(String(viewGameID));
+        await tx.wait();
+      } catch (err) {
+        console.error(err);
+      }
+    };
+  
+    const cancelGame = async () => {
+      try {
+        const signer = await getProviderOrSigner(true);
+  
+        const moneymatchesContract = new Contract(
+          MONEYMATCHES_ADDRESS,
+          abi,
+          signer
+        );
+  
+        const tx = await moneymatchesContract.cancelGameBeforeItHasBeenAccepted(String(viewGameID));
+        await tx.wait();
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+  /*
+    connectWallet: Connects the MetaMask wallet
+  */
+  const connectWallet = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await getProviderOrSigner();
+      setWalletConnected(true);
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const createGameButton = () => {
+    if (walletConnected) {
+        return (
+          <button onClick={createGame} className={styles.button}>
+            Create game
+          </button>
+        );
+    } else {
+      return (
+        (null)
+      );
+    }
+  };
+
+
+    const viewGameButton = () => {
+      if (walletConnected) {
+          return (
+            <button onClick={viewGame} className={styles.button}>
+              View Game Details
+            </button>
+          );
+      }
+    };
+
+    const acceptGameButton = () => {
+      if (walletConnected) {
+          return (
+            <button onClick={acceptGame} className={styles.button}>
+              Accept game
+            </button>
+          );
+      } else {
+        return (
+          <button onClick={connectWallet} className={styles.button}>
+            Connect your wallet
+          </button>
+        );
+      }
+    };
+
+    const cancelGameButton = () => {
+      if (walletConnected) {
+          return (
+            <button onClick={cancelGame} className={styles.button}>
+              Cancel game
+            </button>
+          );
+      } else {
+        return (
+          <button onClick={connectWallet} className={styles.button}>
+            Connect your wallet
+          </button>
+        );
+      }
+    };
+  
+    const settleGameButton = () => {
+      if (walletConnected) {
+          return (
+            <button onClick={settleGame} className={styles.button}>
+              Settle game
+            </button>
+          );
+      } else {
+        return (
+          <button onClick={connectWallet} className={styles.button}>
+            Connect your wallet
+          </button>
+        );
+      }
+    };
+
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Money Matches</title>
+        <meta name="description" content="moneymatches dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>This is a moneymatches dapp</h1>
+          <div className={styles.description}>
+            It's an onchain pvp framework.
+          </div>
+          <input
+            placeholder="Wager"
+            onChange={wagerAmount}
+            className={styles.input}
+            />
+          {createGameButton()}
+          <input
+            placeholder="Game ID"
+            onChange={gameID}
+            className={styles.input}
+            />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+          {viewGameButton()}
+          {acceptGameButton()}
+          {cancelGameButton()}
+          {settleGameButton()}
+      </div>
     </div>
-  )
+  );
 }
